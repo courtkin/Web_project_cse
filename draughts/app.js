d554a64ff58ccc1c914171d900bacb55c6a65b5e
courtkin <artunboz@outlook.com>
2021-01-20 19:04:32 +0300

    added the made change for server side code, which I forgot to save in the last push

--- draughts/app.js
+++ draughts/app.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + "/public"));
 
 /* Add the stats later, for now only the plash screen with no templates. */
 app.get("/", function (req, res, next) {
-  res.sendFile("splash.html", {root: "./public"});
+    res.sendFile("splash.html", {root: "./public"});
 });
 
 app.get("/splash", indexRouter);
@@ -26,26 +26,64 @@ const wss = new websocket.Server({server});
 
 var websockets = {}; //property: websocket, value: game
 
-/*
- * regularly clean up the websockets object
- */
-setInterval(function() {
-  for (let i in websockets) {
-    if (Object.prototype.hasOwnProperty.call(websockets,i)) {
-      let gameObj = websockets[i];
-      //if the gameObj has a final status, the game is complete/aborted
-      if (gameObj.finalStatus != null) {
-        delete websockets[i];
-      }
-    }
-  }
-}, 50000);
+// /*
+//  * regularly clean up the websockets object
+//  */
+// setInterval(function() {
+//   for (let i in websockets) {
+//     if (Object.prototype.hasOwnProperty.call(websockets,i)) {
+//       let gameObj = websockets[i];
+//       //if the gameObj has a final status, the game is complete/aborted
+//       if (gameObj.finalStatus != null) {
+//         delete websockets[i];
+//       }
+//     }
+//   }
+// }, 50000);
 
 var currentGame = new Game(gameStatus.gamesInitialized++);
 var connectionID = 0;
 
 wss.on("connection", function connection(ws) {
-    
+    let con = ws;
+    con.id = connectionID++;
+    let playerType = currentGame.addPlayer(con);
+    websockets[con.id] = currentGame;
+
+    if (playerType == "BLACK") {
+        con.send(messages.S_PLAYER_BLACK);
+    } else {
+        con.send(messages.S_PLAYER_WHITE);
+    }
+
+    if (currentGame.hasTwoConnectedPlayers()) {
+        currentGame = new Game(gameStatus.gamesInitialized++);
+	}
+	
+	con.on("message", function incoming(message) {
+		let objectMessage = JSON.parse(message);
+		let gameObj = websockets[con.id];
+
+		if (objectMessage.type == messages.T_GAME_WON_BY) {
+			let winnerMsg = messages.O_GAME_OVER;
+			winnerMsg.data = objectMessage.data;
+			gameObj.playerBlack.send(JSON.stringify(winnerMsg));
+			gameObj.playerWhite.send(JSON.stringify(winnerMsg));
+			gameObj.setStatus(objectMessage.data);
+		} else if (objectMessage.type == messages.T_BLACK_PLAYED) {
+			let nextTurnMsg = messages.O_NEXT_TURN;
+			nextTurnMsg.data = "WHITE";
+			gameObj.playerBlack.send(JSON.stringify(nextTurnMsg));
+			gameObj.playerWhite.send(JSON.stringify(nextTurnMsg));
+			gameObj.setStatus("WHITE TURN");
+		} else if (objectMessage.type == messages.T_WHITE_PLAYED) {
+			let nextTurnMsg = messages.O_NEXT_TURN;
+			nextTurnMsg.data = "BLACK";
+			gameObj.playerBlack.send(JSON.stringify(nextTurnMsg));
+			gameObj.playerWhite.send(JSON.stringify(nextTurnMsg));
+			gameObj.setStatus(" TURN");
+		}
+	});
 });
 
 server.listen(port);
